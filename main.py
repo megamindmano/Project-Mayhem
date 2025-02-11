240542fc7cba452f0a8794b973559a08a3173cae
megamindmano <48312233+megamindmano@users.noreply.github.com>
2025-02-11 01:16:20 +0000

    Coding working

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ from numpy import vectorize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-sample_files = [doc for doc in os.listdir('sample') if doc.endswith('.txt')]
+sample_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
 sample_contents = [open(File).read() for File in sample_files]
 
 vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
 similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
 
-vectors = vectorize(sample_contents)
-s_vectors = list(zip(sample_files, vectors))
+vector = vectorize(sample_contents)
+s_vectors = list(zip(sample_files, vector))
 
 def check_plagiarism():
-    plagiarism_results = set()
-    for Title1, Text1 in s_vectors:
+    results = set()
+    global s_vectors
+    for sample_a, text_vector_a in s_vectors:
         new_vectors = s_vectors.copy()
-        current_index = new_vectors.index((Title1, Text1))
+        current_index = new_vectors.index((sample_a, text_vector_a))
         del new_vectors[current_index]
-        for Title2, Text2 in new_vectors:
-            sim_score = similarity(Text1, Text2)[0][1]
-            if sim_score > 0.75:
-                plagiarism_results.add((Title1, Title2))
-    return plagiarism_results
+        for sample_b, text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            sample_pair = sorted((sample_a, sample_b))
+            score = sample_pair[0], sample_pair[1], sim_score
+            results.add(score)
+    return results
 
-for Title1, Title2 in check_plagiarism():
-    print(f'{Title1} is similar to {Title2}')
-    
+for data in check_plagiarism():
+    print(data)
